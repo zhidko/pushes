6557f837983ecdf3d68037ee0b7168396e000ba9
Ruslan Zhidko <ruslan.zhidko@together.com>
2017-11-15 11:09:31 +0200

    asdf

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -3,7 +3,7 @@ importScripts('https://www.gstatic.com/firebasejs/3.6.8/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/3.6.8/firebase-messaging.js');
 
 firebase.initializeApp({
-    messagingSenderId: 'AAAAAkigOU0:APA91bHD8LuIpPOv0Wov7_HRBXqf1tL8cuOW5zeQs7hCrrHJEpQZlINWVGLAh8_80ZiCZ3PUxhuyCZ5YORvgM4GCofrnUFEbBbwP71UYIOWQJYSgBgFWixinrFGEPwduy-m3Qo85GFM-'
+    messagingSenderId: '9808394573'
 });
 
 const messaging = firebase.messaging();
