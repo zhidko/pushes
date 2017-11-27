bec87e5e76e48f71acf55b130dc6113b2cb40a99
Ruslan Zhidko <ruslan.zhidko@together.com>
2017-11-27 13:57:06 +0200

    asf

--- firebase-messaging-sw.js
+++
@@ -1,9 +0,0 @@
-// firebase-messaging-sw.js
-importScripts('https://www.gstatic.com/firebasejs/3.6.8/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/3.6.8/firebase-messaging.js');
-
-firebase.initializeApp({
-    messagingSenderId: '9808394573'
-});
-
-const messaging = firebase.messaging();
